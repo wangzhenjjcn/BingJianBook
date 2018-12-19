136bb20ec0fb9f9386e748fb8e0e940584f0a44f
WangZhen <wangzhenjjcn@gmail.com>
2018-12-19 18:01:53 +0800

    init

---
+++ book/src/main/java/com/bingjian/book/App.java
@@ -0,0 +1,13 @@
+package com.bingjian.book;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
